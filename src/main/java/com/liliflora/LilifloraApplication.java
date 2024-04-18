d4a7c559d96eab69254fb635571f81d4cae12c6c
jyo <qwer790488@gmail.com>
2024-04-18 19:42:46 +0900

    first commit

---
+++ src/main/java/com/liliflora/LilifloraApplication.java
@@ -0,0 +1,13 @@
+package com.liliflora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilifloraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilifloraApplication.class, args);
+	}
+
+}
